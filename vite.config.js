2bed2d2672a5ed85882a1057c387ea77c87e5986
Manabu Yoshida <manavu666@gmail.com>
2025-12-20 22:21:18 +0900

    fix: npm run dev が動作しなかった不具合を修正

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0', // Dockerのポートマッピング設定と合わせるためにポートを固定する
+        port: 5173,
+        hmr: {
+            host: 'localhost',
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
